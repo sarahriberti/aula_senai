c10023c734272630018118e329474a91a965d2e5
gian <126879472+GianSenai@users.noreply.github.com>
2023-09-29 10:09:07 -0300

    x

--- reacttt/Opus_React/src/components/Menu_Lateral.jsx
+++ reacttt/Opus_React/src/components/Menu_Lateral.jsx
@@ -1,39 +1,43 @@
-//Inicio MenuLateral
-import { useState } from 'react'; //Importar o react
-import Button from 'react-bootstrap/Button'; //Importar o botão do bootstrap
-import Offcanvas from 'react-bootstrap/Offcanvas'; //Importar o Offcanvas do bootstrap
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Sair from './Sair';
 
-function MenuLateral() { //Função para chamar o MenuLateral
+function MenuLateral() {
   const [show, setShow] = useState(false);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-    <div className='parte_superior'> {/*div que contem o botão e a imagem da logo */}
-    <Button className='chamar_menu' onClick={handleShow}> {/*botão para chamar o modal*/}
-        <img src="./src/assets/menu-aberto (4).png" alt="menu icon" width={50} />
-      </Button>
-    </div> {/*fim da div chamar_menu*/}
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header className='fechar_menu' closeButton></Offcanvas.Header> {/*header do menu*/}
-        <div className='perfil'> {/*div com as informações do perfil*/}
-        <Offcanvas.Body>
-        <img src="https://cdn-icons-png.flaticon.com/128/848/848006.png" alt="user" width={100} className='imagem1'/>
+      <div className='parte_superior'>
+        <Button className='chamar_menu' variant="primary" onClick={handleShow}>
+          <img src="./src/assets/menu-aberto (4).png" alt="menu icon" width={50} />
+        </Button>
+      </div>
+
+      <Offcanvas show={show} onHide={handleClose} backdrop="static">
+        <Offcanvas.Header className='fechar_menu' closeButton></Offcanvas.Header>
+        <div className='perfil'>
+          <Offcanvas.Body>
+          <img src="https://cdn-icons-png.flaticon.com/128/848/848006.png" alt="user" width={100} className='imagem1'/>
           <p className='name'><b>Nome</b></p>
           <a href=""><p>Calendário</p></a>
           <a href=""><p>Adicionar Tarefa</p></a>
           <a href=""><p>Editar Perfil</p></a>
+          <Sair />
           <div className='redes'>
             <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png" alt="insta" className='insta' />
             <img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png" alt="face" className='face' />
             <img src="https://cdn-icons-png.flaticon.com/128/4494/4494494.png" alt="whats" className='whats' />
           </div>
-        </Offcanvas.Body>
+          </Offcanvas.Body>
+        </div>
 
-        </div> {/*fim da div parte_superior*/}
       </Offcanvas>
     </>
   );
 }
+
 export default MenuLateral;
---
+++ reacttt/Opus_React/src/components/Sair.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function Sair() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow} className='linke'>
+        Sair da Conta
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Body>
+          Você deseja realmente sair?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            NÃO
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            SIM
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Sair;
