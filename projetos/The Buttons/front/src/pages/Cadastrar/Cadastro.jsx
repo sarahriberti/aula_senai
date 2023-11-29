2347ef9bc05870b19c3fc3321d8cf56bcdb4ac40
Arthur Vitorio <arthurantoniovitorio@gmail.com>
2023-11-29 15:43:07 -0300

    s
    s

--- projetos/The Buttons/front/src/pages/Cadastrar/Cadastro.jsx
+++ projetos/The Buttons/front/src/pages/Cadastrar/Cadastro.jsx
@@ -2,8 +2,8 @@ import './Cadastro.css'
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 
-const Cadastro = () => {
 
+const Cadastro = () => {
     const [formValues, setFormValues] = useState({
         nome: '',
         email: '',
@@ -13,10 +13,8 @@ const Cadastro = () => {
         confirmsenha:'',
     });
 
-
     const handleChange = (e) => {
         const { name, value } = e.target;
-
         setFormValues((prevValues) => ({
             ...prevValues,
             [name]: value,
@@ -35,7 +33,6 @@ const Cadastro = () => {
                 body: JSON.stringify(formValues),
             });
 
-            // Verifica se os dados foram bem-sucedidos
             if (response.ok) {
                 console.log('Dados enviados com sucesso!');
             } else {
@@ -45,6 +42,7 @@ const Cadastro = () => {
             console.error('Erro na requisição:', error);
         }
     }
+
     return (
         <>
             <main className='container_cad'>
@@ -53,28 +51,40 @@ const Cadastro = () => {
                     <div className="formulario">
                         <h1>Cadastro</h1>
 
-                        {/* Formulário para cadastro*/}
                         <form onSubmit={handleSubmit} id='form_cadastro'>
                             <div className="class_nome">
                                 <label htmlFor="">Nome</label><br />
-                                {/* Nome do usuário */}
                                 <input type="text" name="nome" className="nome" id="nome" placeholder="Digite seu Nome" value={formValues.nome} onChange={handleChange} required />
                             </div>
-
-                            {/* Botões para enviar e cancelar o formulário dp cadastro */}
+                            <div className="class_date">
+                                <label htmlFor="" >Data de nascimento</label>
+                                <input type="date" name="dataNascimento" className="dataNascimento" id="dataNascimento" value={formValues.dataNascimento} onChange={handleChange} required/>
+                            </div>
+                            <div className="class_cel">
+                                <label htmlFor="">Celular</label>
+                                <input type="tel" name="celular" className="celular" id="celular" placeholder="+55/Digite seu número" value={formValues.celular} onChange={handleChange}  required />
+                            </div>
+                            <div className="class_email">
+                                <label htmlFor="">E-mail</label>
+                                <input type="email" name="email" className="email" id="email" placeholder="Digite seu E-mail" value={formValues.email} onChange={handleChange} required />
+                            </div>
+                            <div className="class_senha">
+                                <label htmlFor="">Senha</label>
+                                <input type="password" name="senha" className="senha" id="senha" value={formValues.senha} onChange={handleChange} required />
+                            </div>
+                            <div className="class_confir">
+                                <label htmlFor="confirmsenha"> Confirmar senha</label>
+                                <input type="password" name="confirmsenha" className="confirmsenha" id="confirmsenha" placeholder="Digite sua senha novamente" data-equal="password" value={formValues.confirmsenha}  onChange={handleChange} required />
+                            </div>
                             <div className="botoes_cad">
                                 <input type="submit" value="Cadastrar-se" id="enter_cad" />
                                 <input type="submit" value="Cancelar" id="cancel_cada" />
                             </div>
-
-                            {/* Link para página do login */}
                             <div className="class_botoes">
                                 <li className="text_return">
                                     <Link to="/Login">Já possui uma conta? Faça login</Link>
                                 </li>
                             </div>
-
-                            {/* Cadastro com o google ou instagram*/}
                             <div className="cadastro">
                                 <h3>Cadastrar-se com</h3>
                                 <a href="https://www.instagram.com/"><input type="button" value="Facebook" /></a>
@@ -84,11 +94,11 @@ const Cadastro = () => {
                         </form>
                     </div>
 
+
                 </section>
+
             </main>
         </>
     )
 }
-
-
 export { Cadastro };
