fed29af2ce8507a3628161fd5cb49ced1158d261
emilybesse <126877786+emilybesse@users.noreply.github.com>
2024-08-29 09:20:23 -0300

    Create backup_BD_20240829.sql

---
+++ projetos/opus/banco/backup_BD_20240829.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 29/08/2024 às 14:19
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `opustask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `calendario`
+--
+-- Erro ao ler a estrutura para a tabela opustask.calendario: #1932 - Table &#039;opustask.calendario&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela opustask.calendario: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `opustask`.`calendario`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `compartilhar`
+--
+-- Erro ao ler a estrutura para a tabela opustask.compartilhar: #1932 - Table &#039;opustask.compartilhar&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela opustask.compartilhar: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `opustask`.`compartilhar`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `doacoes`
+--
+-- Erro ao ler a estrutura para a tabela opustask.doacoes: #1932 - Table &#039;opustask.doacoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela opustask.doacoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `opustask`.`doacoes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sugestao`
+--
+-- Erro ao ler a estrutura para a tabela opustask.sugestao: #1932 - Table &#039;opustask.sugestao&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela opustask.sugestao: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `opustask`.`sugestao`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tarefas`
+--
+-- Erro ao ler a estrutura para a tabela opustask.tarefas: #1932 - Table &#039;opustask.tarefas&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela opustask.tarefas: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `opustask`.`tarefas`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+-- Erro ao ler a estrutura para a tabela opustask.usuario: #1932 - Table &#039;opustask.usuario&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela opustask.usuario: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `opustask`.`usuario`&#039; na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
